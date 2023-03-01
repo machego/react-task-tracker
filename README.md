f4c4f0b06f769674142ecbed0f40bc783954f1c6
machego <114348981+machego@users.noreply.github.com>
2023-03-01 20:56:11 +0100

    first version

---
+++ README.md
@@ -0,0 +1,19 @@
+React Crash Course (Task Tracker App)
+
+This is the project from the YouTube crash course(Brad Travesy).
+It includes the react ui as well as JSON-server for our mock backend
+
+Usage
+
+Install dependencies
+
+npm install
+Run React dev server (http://localhost:3000)
+
+npm start
+Run the JSON server (http://localhost:5000)
+
+npm run server
+To build for production
+
+npm run build
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import {useState} from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at Uni',
+        day: 'Feb 6th at 3:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Shopping',
+        day: 'Feb 5th at 6:30pm',
+        reminder: false,
+    },
+]) 
+
+   return (
+     <div className='container'>
+       <Header />
+       <Tasks tasks= {tasks} />
+     </div>
+   )
+ }
+
+
+
+export default App
